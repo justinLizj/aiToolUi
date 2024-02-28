2bd2c97042968c2ed0400cabfe400f72bc99d0f0
justinLizj <126741893+justinLizj@users.noreply.github.com>
2024-02-28 23:03:00 +0800

    备注

--- aiToolUi/vite.config.js
+++ aiToolUi/vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  devServer: {
+    host: '0.0.0.0', // 设置为0.0.0.0表示任何IP都能访问
+    port: 8080 // 自定义端口号
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
